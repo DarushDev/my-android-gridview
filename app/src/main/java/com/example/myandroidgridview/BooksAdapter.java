a349f31dde49e25cefc0e965adc038cf3772a9b2
DarushDev <darushfaqiri@gmail.com>
2017-07-21 19:56:35 +0800

    Populate books into gried view

--- app/src/main/java/com/example/myandroidgridview/BooksAdapter.java
+++ app/src/main/java/com/example/myandroidgridview/BooksAdapter.java
@@ -1,9 +1,11 @@
 package com.example.myandroidgridview;
 
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class BooksAdapter extends BaseAdapter {
@@ -39,9 +41,26 @@ public class BooksAdapter extends BaseAdapter {
     @Override
     public View getView(int position, View view, ViewGroup viewGroup) {
 
-        TextView tvDummy = new TextView(mContext);
-        tvDummy.setText(String.valueOf(position));
-        return tvDummy;
+        // 1
+        final Book book = books[position];
+
+        // 2
+        if(view == null){
+            view = LayoutInflater.from(mContext).inflate(R.layout.book_layout, null);
+        }
+
+        // 3
+        final ImageView imageView = view.findViewById(R.id.imageview_cover_art);
+        final TextView nameTextView = view.findViewById(R.id.textview_book_name);
+        final TextView authorTextView = view.findViewById(R.id.textview_book_author);
+        final ImageView imageViewFavorite = view.findViewById(R.id.imageview_favorite);
+
+        // 4
+        imageView.setImageResource(book.getImageResource());
+        nameTextView.setText(book.getName());
+        authorTextView.setText(book.getAuthor());
+
+        return view;
 
     }
 }
